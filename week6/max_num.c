1b91b88d9c4af3e26a8cbd069f06a9d13960c4d1
dtruong46me <dnhtruong46.cs.pyth@gmail.com>
2024-03-27 17:47:00 +0700

    add max_num.c

--- week6/max_num.c
+++ week6/max_num.c
@@ -1,1 +1,15 @@
-#include<omp.h>
+#include <omp.h>
+#include <stdio.h>
+
+int main(int nargs, char **args) {
+    printf("I love you very much\n");
+
+    #pragma omp parallel
+    {
+        int num_threads, thread_id;
+        num_threads = omp_get_num_threads();
+        thread_id = omp_get_thread_num();
+
+        printf("Hello, thread No.%d of %d threads\n\n", thread_id, num_threads);
+    }
+}
